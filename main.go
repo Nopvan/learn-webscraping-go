edbef32ed7ad83e13499f87f61d161755958d383
NovanWisnu <novanmantab@gmail.com>
2024-04-05 16:03:17 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+type Item struct {
+	Link    string `json:"link"`
+	Name    string `json:"name"`
+	Price   string `json:"price"`
+	Instock string `json:"instock"`
+}
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v", name, time.Since(start))
+	}
+}
+
+func main() {
+	defer timer("time")()
+
+	c := colly.NewCollector()
+
+	items := []Item{}
+
+	c.OnHTML("li.next a", func(h *colly.HTMLElement) {
+		c.Visit(h.Request.AbsoluteURL(h.Attr("href")))
+	})
+
+	c.OnHTML("article.product_pod", func(h *colly.HTMLElement) {
+		i := Item{
+			Link:    h.ChildAttr("a", "href"),
+			Name:    h.ChildAttr("h3 a", "title"),
+			Price:   h.ChildText("p.price_color"),
+			Instock: h.ChildText("p.instock"),
+		}
+		items = append(items, i)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("visiting", r.URL)
+	})
+
+	//set url website
+	c.Visit("https://books.toscrape.com/catalogue/page-1.html")
+
+	//make to json file
+	data, err := json.Marshal(items)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	os.WriteFile("books.json", data, 0644)
+}
